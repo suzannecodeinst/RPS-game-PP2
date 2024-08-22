383812dfd97ef59b51be869b1c78f9c5bf657c57
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-08-22 10:31:37 +0000

    add initial functions to structure js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,17 @@ const buttons = document.getElementsByClassName("btn-choice");
 const userScore = document.getElementById("user-score");
 const puterScore = document.getElementById("puter-score");
 const choices = ["rock", "paper", "scissors"];
+
+// Add initial functions - reference Love Maths video- first setup of js
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
