c849e56691fa0f54d9803af51b025e6479401ccb
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-08-22 10:11:51 +0000

    add hover and click css to user buttons

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+
+const buttons = document.getElementsByClassName("btn-choice");
+const userScore = document.getElementById("user-score");
+const puterScore = document.getElementById("puter-score");
+const choices = ["rock", "paper", "scissors"];
