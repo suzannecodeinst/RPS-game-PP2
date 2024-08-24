21ee25fb55f6e5db1c14aff0f34ba9dc7de3ff5e
Suzanne Deakin <snfa4gse@students.codeinstitute.net>
2024-08-24 14:16:18 +0000

    add js to compare choices and declare winner for a draw.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,39 +3,54 @@
 const buttons = document.getElementsByClassName("btn-choice")
 const puterChoiceDisplay = document.getElementById('puter-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
-const resultDisplay = document.getElementById('result');
 const choices = ["rock", "paper", "scissors"];
 
 
+/*  play game function records data choice value of selected button
+prints array value to user choice*/
+
 
 for (let button of buttons) {
-    button.addEventListener("click", function () {
+  button.addEventListener("click", function () {
     let playerChoice = this.getAttribute("data-choice");
     userChoiceDisplay.innerHTML = `${choices[playerChoice]}`;
     playGame(playerChoice);
+   
   });
 }
 
-/* function records data choice value of selected button
-prints array value to user choice
-
-computer randomly selects a value between 0 and 3
-and selects a corresponding data choice based on array value, prints to computer choice
-
-compares the data choices and declares a winner */
 
 
-  function playGame(playerChoice) {
+/*computer randomly selects a value between 0 and 3
+and selects a corresponding data choice based on array value, prints to computer choice*/
 
-   
-    
-    let puterChoice = Math.floor(Math.random() * 3);
 
-    puterChoiceDisplay.innerHTML = `${choices[puterChoice]}`;
+function playGame(playerChoice) {
 
-    let resultDisplay = checkWinner(choices[puterChoice], choices[playerChoice]);
+  let puterChoice = Math.floor(Math.random() * 3);
+
+  puterChoiceDisplay.innerHTML = `${choices[puterChoice]}`;
+
+  /*compares the data choices and declares a winner */
+
+  let result = document.getElementById("result-display");
+
+  if (puterChoice == playerChoice) {
+    result.innerText = "Draw!";
+
+  } else if (puterChoice == "0") {
+    if (playerChoice == "3") {
+      result.innerText = "Oops you lose!";
+    } else {
+      result.innerText = "Yay you win!";
+    }
+  }  
+
 
   
-  }
+}
 
- 
+
+
+
+
